ed90caf035a2d03271ad0bd4a46e3fcb72126cad
Devon Tolbert <37005487+SlayerA321@users.noreply.github.com>
2022-07-27 16:55:48 -0400

    New m!forecast & m!time commands
    1. Can now use m!forecast to get the predicted weather for the next 6 days for that location
    2. Can now use m!time to get the current time and day
    3. Created embeds for both the weather and forcast commands so that they look prettier (gave colors as well)
    4. Can now add multiple parameters to both m!weather and m!forecast to get accurate searches for cities
    5. Added footers and PNG icon's to embeds!

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+from msilib.schema import LaunchCondition
 import pafy
 import discord
 import os
@@ -8,6 +9,7 @@ from discord.ext import commands
 import random as rand
 from itertools import cycle, islice
 import requests, json
+import datetime
 song_queue = []
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -284,8 +286,8 @@ class Music(commands.Cog):
     await self.queue(ctx)
 
   @commands.command()
-  async def weather(self,ctx,city=""):
-    current_city = str(city)
+  async def weather(self,ctx,city='',city_second_space='',city_third_space=''):
+    current_city = str(city) + " " + str(city_second_space) + " " + city_third_space
     if city == '' or city is None:
         await ctx.send(":person_facepalming: Nothing is a city? :person_facepalming:")
         return None
@@ -300,9 +302,41 @@ class Music(commands.Cog):
         humidity = opendata["humidity"]
         weather = data["weather"]
         desc = weather[0]["description"]
-        await ctx.send("High: *{}°*, low: *{}°*\nCurrent Temp: **{}°**\nHumidity: {}\nDescription: {}".format(str(temp_max), str(temp_min), str(temp), str(humidity), str(desc)))
+        g = discord.Embed(title=":white_sun_cloud: _**{}**_:thunder_cloud_rain:".format(current_city), description="High: *{}°*, low: *{}°*\nCurrent Temp: **{}°**\nHumidity: {}\nDescription: {}".format(str(temp_max), str(temp_min), str(temp), str(humidity), str(desc)), color=discord.Color.dark_blue()).set_footer(text='For a forecast 5 days in advance -> m!forecast', icon_url='https://i.imgur.com/oWQeUDj.png')
+        await ctx.send(embed=g)
     else:
         await ctx.send(":person_facepalming: That isn't a city :person_facepalming:")
+
+  @commands.command()
+  async def forecast(self,ctx,city='',city_second_space='',city_third_space=''):
+    current_city = str(city) + " " + str(city_second_space) + " " + city_third_space
+    if city == '' or city is None:
+        await ctx.send(":person_facepalming: Nothing is a city? :person_facepalming:")
+        return None
+    lonLatCall = "http://api.openweathermap.org/geo/1.0/direct?" +"q=" + current_city + "&appid=" + weather_key
+    response = requests.get(lonLatCall)
+    data_first_call = response.json()
+    lon = data_first_call[0]["lon"]
+    lat = data_first_call[0]["lat"]
+    latt = str(lat)
+    lonn = str(lon)
+    forecast_call = "http://api.openweathermap.org/data/2.5/forecast?" + "appid=" + weather_key + "&lat=" + latt + "&lon=" + lonn + "&units=imperial"
+    forecast_response = requests.get(forecast_call)
+    for_data = forecast_response.json()
+    if for_data["cod"] != "404":
+      forecast = for_data["list"]
+      temp = ""
+      tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+      for x in range(6):
+        temp += "**{}**\n:thermometer:High: {} - Low: {} - Avg: **{}**°\n sky: *{}*\n\n".format(tomorrow + datetime.timedelta(days=x), forecast[x]["main"]["temp_max"], forecast[x]["main"]["temp_min"], forecast[x]["main"]["temp"], forecast[x]["weather"][0]["description"])
+      g = discord.Embed(title=":white_sun_cloud: _**{}**_'s Expected Weather (Next 6 days) :thunder_cloud_rain:".format(current_city.capitalize()), description=temp, color=discord.Color.dark_blue()).set_footer(text='Want the current temperature? use m!weather', icon_url='https://i.imgur.com/oWQeUDj.png').set_thumbnail(url='https://i.imgur.com/j7WQ7bx.png')
+      await ctx.send(embed=g)
+    else:
+        await ctx.send(":person_facepalming: That isn't a city :person_facepalming:")
+
+  @commands.command()
+  async def time(self,ctx):
+    await ctx.send(":clock2: *{}*".format(datetime.datetime.now().strftime("%I:%M %p on %A, %b %d")))
 
   async def playlist(self, ctx, url):
     playlist = pafy.get_playlist2(url)
